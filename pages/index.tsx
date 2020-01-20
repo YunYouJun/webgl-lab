c4a5dd716a913ad9ed3c0beeecd7eb2e2f7af2f4
YunYouJun <me@yunyoujun.cn>
2020-01-20 23:43:19 +0800

    next

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,62 @@
-import Link from 'next/link';
-import './index.css';
+import Link from "next/link";
+import { NextPage } from "next";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="/logo.svg" className="App-logo" alt="logo" />
-        <p>
-          The lab space for learning WebGL. (deving...)
-        </p>
-        <h2>
-          WebGL Lab
-        </h2>
-        <p>
-          <Link href="/bezier-curve">
-            <a>Bézier curve</a>
-          </Link>
-          <br />
-          <Link href="/model-view">
-            <a>Model View</a>
-          </Link>
-        </p>
-      </header>
-    </div>
-  );
+function getExamples() {
+  return [
+    { id: "bezier-curve", title: "Bézier curve" },
+    { id: "model-view", title: "Moodel View" },
+    { id: "hello-canvas", title: "Hello Canvas" }
+  ];
 }
 
-export default App;
+const ExampleLink = ({ example }) => (
+  <li key={example.id}>
+    <Link href={`/${example.id}`}>
+      <a>{example.title}</a>
+    </Link>
+  </li>
+);
+
+const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
+  <div className="App">
+    <img src="/logo.svg" className="App-logo" alt="logo" />
+    <h2>WebGL Lab</h2>
+    <p>
+      The lab space for learning WebGL. (deving...)
+      <br />
+      User Agent: {userAgent}
+    </p>
+    <ul>
+      {getExamples().map(example => (
+        <ExampleLink key={example.id} example={example} />
+      ))}
+    </ul>
+    <style jsx global>{`
+      body {
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+          "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+          "Helvetica Neue", sans-serif;
+      }
+
+      code {
+        font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+          monospace;
+      }
+
+      .App {
+        text-align: center;
+      }
+
+      .App-logo {
+        height: 40vmin;
+      }
+    `}</style>
+  </div>
+);
+
+Home.getInitialProps = async ({ req }) => {
+  const userAgent = req ? req.headers["user-agent"] || "" : navigator.userAgent;
+  return { userAgent };
+};
+
+export default Home;
---
+++ pages/model-view.tsx
@@ -0,0 +1,21 @@
+// import ModelViewer from '@google/model-viewer';
+
+export default function ModelView() {
+  return (
+    <div style={{ background: "#ECECEC", padding: "30px" }}>
+      {/* <ModelViewer
+        src="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b/Astronaut.glb?1542147958948"
+        ios-src="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b/Astronaut.usdz?v=1569545377878"
+        alt="A 3D model of an astronaut"
+        auto-rotate
+        background-color="#70BCD1"
+        shadow-intensity="1"
+        camera-controls
+        interaction-prompt="auto"
+        ar
+        magic-leap
+        style={{ height: 600, outline: "none" }}
+      ></ModelViewer> */}
+    </div>
+  );
+}
